316cde5eb3fc607f82afaac075f7393d279ee35e
klauz <klauzwayne@gmail.com>
2022-03-05 11:02:09 +0100

    created masterMission subclass

---
+++ Source/ContractsWindow/PanelInterfaces/masterMission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContractsWindow.PanelInterfaces {
+    class masterMission : contractMission {
+        private const string masterMissionTitle = "MasterMission";
+        public new bool MasterMission {
+            get {
+                return true;
+            }
+            set {
+            }
+        }
+
+        internal masterMission() : base(masterMissionTitle) {
+        }
+
+        internal masterMission(string active, string hidden, string vessels, bool asc, bool showActive, int sMode) : base(masterMissionTitle, active, hidden, vessels, asc, showActive, sMode, true) {
+        }
+    }
+}
